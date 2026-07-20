41bccb809a9caa5e9a92d0f5c6de6a334476cbf2
ANCHIT KUMAR <anchitkumar94@gmail.com>
2026-07-21 00:37:24 +0530

    Add test for SendMailToCustomer method

--- Week-1-Engineering-Concepts/NUnit and Moq/Moq/Exercise-1-Mock-MailSender/CustomerComm.Tests/CustomerCommTests.cs
+++ Week-1-Engineering-Concepts/NUnit and Moq/Moq/Exercise-1-Mock-MailSender/CustomerComm.Tests/CustomerCommTests.cs
@@ -16,4 +16,16 @@ public class CustomerCommTests
         _mockMailSender = new Mock<IMailSender>();
         _customerComm = new CustomerComm(_mockMailSender.Object);
     }
+
+    [Test]
+    public void SendMailToCustomer_ShouldReturnTrue_WhenMailIsSent()
+    {
+        _mockMailSender
+            .Setup(x => x.SendMail(It.IsAny<string>(), It.IsAny<string>()))
+            .Returns(true);
+
+        bool result = _customerComm.SendMailToCustomer();
+
+        Assert.That(result, Is.True);
+    }
 }
