2704e1cc8182fd21e2e685780eaa3bcd2dabdcee
ANCHIT KUMAR <anchitkumar94@gmail.com>
2026-07-20 23:20:05 +0530

    Implement cursor to iterate employee records

---
+++ Week-1-Engineering-Concepts/AdvancedSQLServer/7. SQL Exercise - Cursors/Exercise-1-CreateCursor.sql
@@ -0,0 +1,53 @@
+
+
+DECLARE
+    @EmployeeID INT,
+    @FirstName VARCHAR(50),
+    @LastName VARCHAR(50),
+    @DepartmentID INT,
+    @Salary DECIMAL(10,2),
+    @JoinDate DATE;
+
+DECLARE EmployeeCursor CURSOR
+FOR
+SELECT
+    EmployeeID,
+    FirstName,
+    LastName,
+    DepartmentID,
+    Salary,
+    JoinDate
+FROM Employees;
+
+OPEN EmployeeCursor;
+
+FETCH NEXT FROM EmployeeCursor
+INTO
+    @EmployeeID,
+    @FirstName,
+    @LastName,
+    @DepartmentID,
+    @Salary,
+    @JoinDate;
+
+WHILE @@FETCH_STATUS = 0
+BEGIN
+    PRINT
+        'EmployeeID: ' + CAST(@EmployeeID AS VARCHAR(10))
+        + ', Name: ' + @FirstName + ' ' + @LastName
+        + ', DepartmentID: ' + CAST(@DepartmentID AS VARCHAR(10))
+        + ', Salary: ' + CAST(@Salary AS VARCHAR(20));
+
+    FETCH NEXT FROM EmployeeCursor
+    INTO
+        @EmployeeID,
+        @FirstName,
+        @LastName,
+        @DepartmentID,
+        @Salary,
+        @JoinDate;
+END;
+
+CLOSE EmployeeCursor;
+DEALLOCATE EmployeeCursor;
+GO
