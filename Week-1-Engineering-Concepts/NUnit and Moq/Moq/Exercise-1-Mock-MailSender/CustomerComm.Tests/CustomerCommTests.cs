ee5ec18266f648abcf89bc8ab88a41de35fd9b0b
ANCHIT KUMAR <anchitkumar94@gmail.com>
2026-07-21 00:35:54 +0530

    Update CustomerCommTests.cs

--- Week-1-Engineering-Concepts/NUnit and Moq/Moq/Exercise-1-Mock-MailSender/CustomerComm.Tests/CustomerCommTests.cs
+++ Week-1-Engineering-Concepts/NUnit and Moq/Moq/Exercise-1-Mock-MailSender/CustomerComm.Tests/CustomerCommTests.cs
@@ -3,3 +3,17 @@ using Moq;
 using NUnit.Framework;
 
 namespace CustomerComm.Tests;
+
+[TestFixture]
+public class CustomerCommTests
+{
+    private Mock<IMailSender> _mockMailSender;
+    private CustomerComm _customerComm;
+
+    [SetUp]
+    public void Setup()
+    {
+        _mockMailSender = new Mock<IMailSender>();
+        _customerComm = new CustomerComm(_mockMailSender.Object);
+    }
+}
